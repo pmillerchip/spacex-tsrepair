03d724fcc7f6d83b93dee7cf8c5f3d758c31053f
Peter Millerchip <pete@millerchipsoftware.com>
2016-10-27 21:07:01 +0100

    Conversion to C++11

--- TSPacket.cpp
+++ TSPacket.cpp
@@ -96,7 +96,7 @@ TSPacket::removeScramble()
 unsigned char*
 TSPacket::adaptationField() const
 {
-  if ((data[3] & 0x20) == 0) return((unsigned char*)0);
+  if ((data[3] & 0x20) == 0) return(nullptr);
   
   return(data + 4);
 }
@@ -113,7 +113,7 @@ bool
 TSPacket::hasPCR() const
 {
   const unsigned char* af = adaptationField();
-  if (af == (const unsigned char*)0) return(false);  // PCR is in AF, and AF is not present
+  if (af == nullptr) return(false);  // PCR is in AF, and AF is not present
   
   return((af[1] & 0x10) != 0);
 }
@@ -123,7 +123,7 @@ void
 TSPacket::removePCR()
 {
   unsigned char* af = adaptationField();
-  if (af == (unsigned char*)0) return;  // PCR is in AF, and AF is not present
+  if (af == nullptr) return;  // PCR is in AF, and AF is not present
   
   af[1] &= ~0x10;
 }
@@ -133,7 +133,7 @@ bool
 TSPacket::hasOPCR() const
 {
   const unsigned char* af = adaptationField();
-  if (af == (const unsigned char*)0) return(false);  // OPCR is in AF, and AF is not present
+  if (af == nullptr) return(false);  // OPCR is in AF, and AF is not present
   
   return((af[1] & 0x08) != 0);
 }
@@ -143,15 +143,15 @@ unsigned long long int
 TSPacket::getPCR() const
 {
   const unsigned char* af = adaptationField();
-  if (af == (const unsigned char*)0) return(false);  // PCR is in AF, and AF is not present
+  if (af == nullptr) return(false);  // PCR is in AF, and AF is not present
   
   unsigned long long int rv =
-    (((unsigned long long int)af[2]) << 40)
-  | (((unsigned long long int)af[3]) << 32)
-  | (((unsigned long long int)af[4]) << 24)
-  | (((unsigned long long int)af[5]) << 16)
-  | (((unsigned long long int)af[6]) << 8)
-  | (((unsigned long long int)af[7]));
+    (static_cast<unsigned long long int>(af[2]) << 40)
+  | (static_cast<unsigned long long int>(af[3]) << 32)
+  | (static_cast<unsigned long long int>(af[4]) << 24)
+  | (static_cast<unsigned long long int>(af[5]) << 16)
+  | (static_cast<unsigned long long int>(af[6]) << 8)
+  | (static_cast<unsigned long long int>(af[7]));
   return(rv);
 }
 
@@ -160,7 +160,7 @@ void
 TSPacket::setPCR(unsigned long long int newPCR)
 {
   unsigned char* af = adaptationField();
-  if (af == (unsigned char*)0) return;  // PCR is in AF, and AF is not present
+  if (af == nullptr) return;  // PCR is in AF, and AF is not present
   
   // Set PCR present bit
   af[1] |= 0x10;
@@ -185,7 +185,7 @@ TSPacket::hasPayload() const
 unsigned char*
 TSPacket::payload() const
 {
-  if ((data[3] & 0x10) == 0) return((unsigned char*)0);
+  if ((data[3] & 0x10) == 0) return(nullptr);
   
   return(data + getPayloadOffset());
 }
@@ -197,9 +197,9 @@ TSPacket::getPayloadOffset() const
   if ((data[3] & 0x10) == 0) return(0);
   
   unsigned char* af = adaptationField();
-  if (af == (unsigned char*)0) return(4);
+  if (af == nullptr) return(4);
   
-  int afLen = (int)af[0];
+  unsigned int afLen = static_cast<unsigned int>(af[0]);
   return(4 + 1 + afLen);
 }
 
@@ -208,7 +208,7 @@ unsigned int
 TSPacket::afLen() const
 {
   unsigned char* af = adaptationField();
-  if (af == (unsigned char*)0) return(0);
+  if (af == nullptr) return(0);
   
   return((unsigned int)af[0]);
 }
@@ -221,7 +221,7 @@ TSPacket::setAFLen(unsigned int newLen)
   data[3] |= 0x20;
 
   unsigned char* af = adaptationField();
-  if (af == (unsigned char*)0) return;
+  if (af == nullptr) return;
   
   af[0] = newLen;
 }
@@ -233,9 +233,9 @@ TSPacket::getPayloadSize() const
   if ((data[3] & 0x10) == 0) return(0);
 
   const unsigned char* af = adaptationField();
-  if (af == (const unsigned char*)0) return(TS_PACKET_SIZE - 4);
+  if (af == nullptr) return(TS_PACKET_SIZE - 4);
   
-  int afLen = (int)af[0];
+  unsigned int afLen = static_cast<unsigned int>(af[0]);
   if (afLen > (TS_PACKET_SIZE - 4 - 1)) return(0);
   return(TS_PACKET_SIZE - afLen - 4 - 1);
 }
@@ -252,7 +252,7 @@ bool
 TSPacket::hasPTS() const
 {
   const unsigned char* pay = payload();
-  if (pay == (const unsigned char*)0) return(false);
+  if (pay == nullptr) return(false);
   
   if ((pay[0] != 0)
    || (pay[1] != 0)
@@ -271,7 +271,7 @@ unsigned long long int
 TSPacket::getPTS() const
 {
   const unsigned char* pay = payload();
-  if (pay == (const unsigned char*)0) return(0);
+  if (pay == nullptr) return(0);
   
   if ((pay[0] != 0)
    || (pay[1] != 0)
@@ -284,11 +284,11 @@ TSPacket::getPTS() const
   // It's a PES packet
   if ((pay[7] & 0x80) == 0) return(0);
   
-  return(((unsigned long long int)(pay[9]  & 0x0e) << 29)
-       | ((unsigned long long int)(pay[10] & 0xff) << 22)
-       | ((unsigned long long int)(pay[11] & 0xfe) << 14)
-       | ((unsigned long long int)(pay[12] & 0xff) << 7)
-       | ((unsigned long long int)(pay[13] & 0xfe) >> 1));
+  return((static_cast<unsigned long long int>(pay[9]  & 0x0e) << 29)
+       | (static_cast<unsigned long long int>(pay[10] & 0xff) << 22)
+       | (static_cast<unsigned long long int>(pay[11] & 0xfe) << 14)
+       | (static_cast<unsigned long long int>(pay[12] & 0xff) << 7)
+       | (static_cast<unsigned long long int>(pay[13] & 0xfe) >> 1));
 }
 
 //----------------------------------------------------------------------------
@@ -296,7 +296,7 @@ void
 TSPacket::setPTS(unsigned long long int newPTS)
 {
   unsigned char* pay = payload();
-  if (pay == (unsigned char*)0) return;
+  if (pay == nullptr) return;
   
   pay[9]  = (pay[9] & 0xf1) | ((newPTS >> 29) & 0x0e);
   pay[10] = (newPTS >> 22) & 0xff;
